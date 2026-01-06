f62e85e7218adb6ea9e21c810167cba33853a894
starcrouz <starcrouz@users.noreply.github.com>
2026-01-06 18:32:31 +0100

    feat: add gallery-10 image and halve marquee animation duration

--- constants.ts
+++ constants.ts
@@ -36,6 +36,7 @@ export const galleryImages = [
     { src: '/assets/images/gallery-7.jpg', w: 1, h: 1 },
     { src: '/assets/images/gallery-8.jpg', w: 1, h: 1 },
     { src: '/assets/images/gallery-9.jpg', w: 2, h: 1 },
+    { src: '/assets/images/gallery-10.png', w: 2, h: 2 },
 ];
 
 export const posters = [
