3a9c8fb1522b7471a4a55c41c896cc28d58415ba
starcrouz <starcrouz@users.noreply.github.com>
2026-09-05 15:05:11 +0200

    feat: add patemma 2026-09-05 poster to constants list

--- constants.ts
+++ constants.ts
@@ -109,6 +109,7 @@ export const galleryImages = [
 ];
 
 export const posters = [
+  '/assets/images/poster-2026-09-05-patemma.jpg',
   '/assets/images/poster-2026-05-15-pouillade.jpg',
   '/assets/images/poster-2026-02-19-piano-vache.jpg',
   '/assets/images/poster-2026-01-17.jpg',
